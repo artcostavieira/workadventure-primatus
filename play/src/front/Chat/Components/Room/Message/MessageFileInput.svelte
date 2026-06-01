cc4443a0227e990030cdadf3152b283bb7b69d73
yannisdia <y.diarra@workadventu.re>
2026-06-01 16:15:31 +0200

    improve file preview

--- play/src/front/Chat/Components/Room/Message/MessageFileInput.svelte
+++ play/src/front/Chat/Components/Room/Message/MessageFileInput.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import { get } from "svelte/store";
-    import { createEventDispatcher, onMount } from "svelte";
+    import { createEventDispatcher, onDestroy, onMount, tick } from "svelte";
     import { selectedChatMessageToReply } from "../../../Stores/ChatStore";
     import { chatInputFocusStore } from "../../../../Stores/ChatStore";
     import { IconLoader, IconPaperclip, IconX } from "@wa-icons";
+    import LL from "../../../../../i18n/i18n-svelte";
 
     const dispatch = createEventDispatcher<{
         filesSelected: FileList;
@@ -12,9 +13,12 @@
 
     let files: FileList | undefined = undefined;
     let fileInputElement: HTMLInputElement;
+    let pickerOpening = false;
+    let pickerOpeningTimeout: ReturnType<typeof setTimeout> | undefined = undefined;
 
     $: {
         if (files && files.length > 0) {
+            stopPickerOpening();
             dispatch("filesSelected", files);
             dispatch("fileUploaded");
             files = undefined;
@@ -29,6 +33,32 @@
         dispatch("fileUploaded");
     }
 
+    function stopPickerOpening() {
+        pickerOpening = false;
+        if (pickerOpeningTimeout) {
+            clearTimeout(pickerOpeningTimeout);
+            pickerOpeningTimeout = undefined;
+        }
+    }
+
+    async function openFilePicker() {
+        if (!fileInputElement) {
+            return;
+        }
+
+        pickerOpening = true;
+        await tick();
+        fileInputElement.click();
+
+        if (pickerOpeningTimeout) {
+            clearTimeout(pickerOpeningTimeout);
+        }
+        pickerOpeningTimeout = setTimeout(() => {
+            pickerOpening = false;
+            pickerOpeningTimeout = undefined;
+        }, 1_000);
+    }
+
     function focusChatInput() {
         // Disable input manager to prevent the game from receiving the input
         chatInputFocusStore.set(true);
@@ -39,9 +69,13 @@
     }
 
     onMount(() => {
-        // Unselect chat message to reply if the input is focused
-        const input = document.getElementById("labelUpload");
-        input?.click();
+        window.addEventListener("focus", stopPickerOpening);
+        openFilePicker().catch((error) => console.error(error));
+    });
+
+    onDestroy(() => {
+        stopPickerOpening();
+        window.removeEventListener("focus", stopPickerOpening);
     });
 </script>
 
@@ -56,18 +90,21 @@
         data-testid="uploadChatCustomAsset"
         on:focusin={focusChatInput}
         on:focusout={unfocusChatInput}
+        on:change={stopPickerOpening}
     />
-    <label
-        id="labelUpload"
-        for="upload"
-        class="p-0 m-0 h-11 w-11 flex items-center justify-center hover:bg-white/10 rounded-none"
+    <button
+        type="button"
+        class="p-0 m-0 h-11 w-11 flex items-center justify-center hover:bg-white/10 rounded-none disabled:opacity-50"
+        aria-label={$LL.chat.fileAttachment.title()}
+        on:click={openFilePicker}
+        disabled={pickerOpening}
     >
-        {#if files !== undefined}
+        {#if pickerOpening}
             <IconLoader class="animate-spin" font-size={18} />
         {:else}
             <IconPaperclip class="hover:!cursor-pointer" font-size={18} />
         {/if}
-    </label>
+    </button>
     <button
         class="absolute top-0 right-0 m-1 hover:bg-white/10 cursor-pointer"
         on:click={() => unselectChatMessageToReplyIfSelected()}
